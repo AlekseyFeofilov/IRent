4c065872baf33b4d29027b4ac36aa94918de464b
Aleksey Feofilov <Alexfil888@gmail.com>
2022-07-08 23:46:13 +0700

    add fragments (favorites, own, settings, rent history, wallet)

--- app/src/main/java/com/example/shift/WalletFragment.kt
+++ app/src/main/java/com/example/shift/WalletFragment.kt
@@ -5,16 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.shift.databinding.FragmentProfileBinding
+import com.example.shift.databinding.FragmentWalletBinding
 
-class ProfileFragment  : Fragment() {
-    private lateinit var binding: FragmentProfileBinding
+class WalletFragment : Fragment() {
+    private lateinit var binding: FragmentWalletBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        binding = FragmentWalletBinding.inflate(inflater, container, false)
 
         return binding.root
     }
