d0b123d94010e416940288a3733e7ddeb1fa910a
Aleksey Feofilov <Alexfil888@gmail.com>
2022-07-08 20:27:33 +0700

    add main fragments (feed, createCard, chat and profile)

---
+++ app/src/main/java/com/example/shift/FeedFragment.kt
@@ -0,0 +1,21 @@
+package com.example.shift
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.shift.databinding.FragmentFeedBinding
+
+class FeedFragment : Fragment() {
+    private lateinit var binding: FragmentFeedBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFeedBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+}
