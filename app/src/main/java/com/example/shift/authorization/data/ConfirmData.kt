aa189a0ccb59fe5cae62d3d18374f46a3984a2e8
Aleksey Feofilov <Alexfil888@gmail.com>
2022-07-23 22:55:04 +0300

    fix authorization bag

--- app/src/main/java/com/example/shift/authorization/data/ConfirmData.kt
+++ app/src/main/java/com/example/shift/authorization/data/ConfirmData.kt
@@ -1,6 +1,10 @@
 package com.example.shift.authorization.data
 
+import com.google.gson.annotations.SerializedName
+
 data class ConfirmData(
+    @SerializedName("tempID")
     val id: Long,
+    @SerializedName("emailCode")
     val code: String
 )
